64d0f187927c8201a92a2f2137ee432da64f97b9
Ethan <ethanhuang2078@gmail.com>
2024-12-23 21:48:52 -0800

    remove unused processVideo function from utils

--- backend/utils.py
+++ backend/utils.py
@@ -18,36 +18,6 @@ def formatSeconds(seconds):
 
     return f"{minutes:02}:{seconds:02}.{milliseconds:03}"
 
-#might be better to make a video class i'm not sure
-def processVideo(filePath):
-    audioPath = filePath.replace(".mp4", ".wav")
-    
-    try:
-        subprocess.run(['ffmpeg', '-i', filePath, '-vn', audioPath,]) # split
-    except:
-        print("Error splitting video to audio")
-        return None
-
-    try:
-        command = [
-            "ffprobe",
-            "-v", "quiet",
-            "-print_format", "json",
-            "-show_format",
-            "-show_streams",
-            filePath
-        ]
-
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=True)
-        f = open(f'./media/json/{name}', 'w')
-        f.write(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error running ffprobe: {e.stderr}")
-        return None
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON: {e}")
-        return None
-
 def displayTimestamp(fps1, fps2, offsetSeconds):
     if (offsetSeconds > 0):
         return f"{fps2.name} occurs at {formatSeconds(offsetSeconds)} in {fps1.name}"
